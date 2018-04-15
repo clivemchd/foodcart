ea7ffd0e175b1c0fca04fcfbae54e65271b3638c
clivemachado <clive.machado@raweng.com>
2018-04-15 19:27:05 +0530

    foodcart: - second commit.

--- app/src/main/java/shop/cart/foodcart/MainActivity.java
+++ app/src/main/java/shop/cart/foodcart/MainActivity.java
@@ -3,6 +3,7 @@ package shop.cart.foodcart;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.util.Log;
@@ -15,8 +16,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Map;
+
+import static android.R.attr.data;
+import static shop.cart.foodcart.R.layout.activity_main;
+import static shop.cart.foodcart.R.layout.content_main;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -27,7 +43,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(activity_main);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -38,6 +54,10 @@ public class MainActivity extends AppCompatActivity
         //get current user
         final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         Log.d("Inside", String.valueOf(user));
+
+        //get firebase realtime database
+        DatabaseReference myRef = FirebaseDatabase.getInstance().getReference();
+//       myRef.child("users").setValue("World!");
 
 
         authListener = new FirebaseAuth.AuthStateListener() {
@@ -53,11 +73,48 @@ public class MainActivity extends AppCompatActivity
             }
         };
 
+
+
+        ValueEventListener postListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Get Post object and use the values to update the UI
+                Map<String, Object> map = (Map<String, Object>) dataSnapshot.getValue();
+
+                long length = (long) map.get("count");
+                for (int i = 0; i < length; i++){
+                    ImageButton imageBtn = new ImageButton(MainActivity.this);
+                    LinearLayout linearLayout = (LinearLayout)findViewById(R.id.linear_layout_main);
+                    ConstraintLayout.LayoutParams lp = new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.MATCH_PARENT, ConstraintLayout.LayoutParams.WRAP_CONTENT);
+                    imageBtn.setImageResource(R.mipmap.meal2_img);
+                    imageBtn.setMaxWidth(140);
+                    imageBtn.setMaxHeight(80);
+                    linearLayout.addView(imageBtn, lp);
+
+//                    LinearLayout.LayoutParams imageParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+//                    imageBtn.setLayoutParams(imageParams);
+
+                }
+                Log.d("success data", "Value is: " +  map.get("count"));
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Getting Post failed, log a message
+                Log.w("faied data", "loadPost:onCancelled", databaseError.toException());
+                // ...
+            }
+
+        };
+        myRef = myRef.child("objects");
+        myRef.addValueEventListener(postListener);
+
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "No Items in Cart", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
@@ -94,20 +151,20 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
@@ -118,7 +175,23 @@ public class MainActivity extends AppCompatActivity
         if (id == R.id.nav_home) {
 
         } else if (id == R.id.nav_cart) {
-
+            FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+            fab.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Snackbar.make(view, "No Items in Cart", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }
+            });
+        } else if (id == R.id.user_orders) {
+            FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+            fab.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Snackbar.make(view, "No Items in Orders", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }
+            });
         } else if (id == R.id.nav_logout) {
             signOut();
         }
